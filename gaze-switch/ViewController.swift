6491991aeb85a647d2cc0594cdd67600e4b51b16
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2017-09-24 15:32:55 +0900

    Initial Commit

---
+++ gaze-switch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gaze-switch
+//
+//  Created by 鎌田遼 on 2017/09/24.
+//  Copyright © 2017年 鎌田遼. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
